c4b173104d2a215f5c340c9e690f068d85c9b452
yunc <mr.yt510@gmail.com>
2023-09-01 16:52:36 +0900

    step10

--- skeleton/step10/handler.go
+++ skeleton/step10/handler.go
@@ -67,13 +67,15 @@ func (hs *Handlers) ListHandler(w http.ResponseWriter, r *http.Request) {
 
 // 保存
 func (hs *Handlers) SaveHandler(w http.ResponseWriter, r *http.Request) {
-	if /* TODO: r.MethodがPOST（http.MethodPost）か調べる */ {
+	if r.Method != http.MethodPost /* TODO: r.MethodがPOST（http.MethodPost）か調べる */ {
 		code := http.StatusMethodNotAllowed
 		http.Error(w, http.StatusText(code), code)
 		return
 	}
 
 	// TODO: フォームから送られてきた品目を取得して、categoryに入れる
+	category := r.FormValue("category")
+
 	if category == "" {
 		http.Error(w, "品目が指定されていません", http.StatusBadRequest)
 		return
@@ -91,7 +93,7 @@ func (hs *Handlers) SaveHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// TODO: itemをデータベースに保存する
-	if /* ここに追加 */; err != nil {
+	if err := hs.ab.AddItem(item) /* ここに追加 */; err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
